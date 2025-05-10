97bb1365e27763dfc1e3e6c80546ec40cd3bb1e6
knm22mukk <knm22mukk@gmail.com>
2025-05-10 10:37:53 +0900

    lefthookの設定導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 export default function Home() {
+  const str: string = 'Hello World';
+  const num: number = 'Hello World';
   return (
     <div>
       <h1 className='font-bold text-red-500'>daikoku infomation</h1>
