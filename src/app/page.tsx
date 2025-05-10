1db1ea428e4f38936dbdf6526ee0d4e3ddb35dd5
knm22mukk <knm22mukk@gmail.com>
2025-05-10 10:43:39 +0900

    lefthookの設定を変更しました

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <h1 className='p-12 font-bold m-3 text-red-500'>daikoku infomation</h1>
+      <h1 className='m-3 font-bold text-red-500 p-12 bg-gray-100'>
+        daikoku infomation
+      </h1>
     </div>
   );
 }
