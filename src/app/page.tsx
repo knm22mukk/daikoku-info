95a068b230b0451f29936a0849c6d083ff41984b
knm22mukk <knm22mukk@gmail.com>
2025-05-10 10:40:58 +0900

    lefthookの動作確認

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
-  const str: string = 'Hello World';
-  const num: number = 'Hello World';
   return (
     <div>
-      <h1 className='font-bold text-red-500'>daikoku infomation</h1>
+      <h1 className='p-12 font-bold m-3 text-red-500'>daikoku infomation</h1>
     </div>
   );
 }
